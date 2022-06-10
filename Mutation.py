c8207e07d67d735094d2c79fc9f45da9c54eb901
rundead3 <74833832+rundead3@users.noreply.github.com>
2022-06-10 17:06:56 +0100

    GA1

--- Mutation.py
+++ Mutation.py
@@ -5,16 +5,16 @@ Created on Sat May 21 11:34:32 2022
 @author: João Correia
 """
 
-
 ### O template mutation é só para ver se o algoritmo funciona, depois adiciona-se operadores
-    
+
 from copy import deepcopy
 from Individuals import Individual
 from random import sample, random, randint
 
+
 def template_mutation(parent1):
     """ Given 1 parent, generates a child
-    
+
     Inputs:
         - parent1: Individual
     Output:
@@ -22,28 +22,30 @@ def template_mutation(parent1):
     """
     return parent1
 
+
 def swap_mutation(parent1):
     """ Given 1 parent, generates a child, which is the same
     as the parent, but with two numbers swapped
-    
+
     Inputs:
         - parent1: Individual
     Output:
         - child1: Individual
     """
     new_representation = deepcopy(parent1.representation)
-    swap_points = sample(range(0, len(new_representation)),2)
-    a,b = swap_points[0], swap_points[1]
+    swap_points = sample(range(0, len(new_representation)), 2)
+    a, b = swap_points[0], swap_points[1]
     new_representation[a], new_representation[b] = new_representation[b], new_representation[a]
     new_indiv = Individual(new_representation)
     return new_indiv
 
+
 def inverse_mutation(parent1):
     new_representation = deepcopy(parent1.representation)
-    swap_points = sample(range(0,len(new_representation)),2)
+    swap_points = sample(range(0, len(new_representation)), 2)
     swap_points.sort()
     chosen_slice = new_representation[swap_points[0]:swap_points[1]]
-    chosen_slice = chosen_slice[::-1]   
+    chosen_slice = chosen_slice[::-1]
     new_representation[swap_points[0]:swap_points[1]] = chosen_slice
     new_indiv = Individual(new_representation)
     return new_indiv
@@ -51,25 +53,20 @@ def inverse_mutation(parent1):
 
 def boundary_mutation(parent1):
     new_representation = deepcopy(parent1.representation)
-    i = randint(0,len(new_representation)-1)
+    i = randint(0, len(new_representation) - 1)
     r = random()
     if r >= 0.5:
-        new_representation[i] = new_representation[i] * new_representation[-1]
+        new_representation[i] = new_representation[i] * 2
     else:
-        new_representation[i] = new_representation[i] *-1.1*new_representation[-1]
+        new_representation[i] = new_representation[i] * -1
     new_indiv = Individual(new_representation)
     return new_indiv
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     a = Individual()
     child = inverse_mutation(a)
     print(a)
     print(child)
 
-
-    
-    
 ### Fazer sempre uma cópia (deepcopy)
-    
--- Population.py
+++ Population.py
@@ -5,14 +5,15 @@ Created on Sat May 21 11:32:30 2022
 @author: João Correia
 """
 
-   
+import matplotlib.pyplot as plt
 from audioop import cross
 import random
 from operator import attrgetter
 from Individuals import *
 from Selection import random_selection, tournament_selection, roulette_selection
-from Crossover import template_crossover, pmx_co, single_point_co ,double_point_co
+from Crossover import template_crossover, pmx_co, single_point_co, double_point_co
 from Mutation import template_mutation, swap_mutation, inverse_mutation, boundary_mutation
+
 
 class Population:
     def __init__(self, pop_size):
@@ -21,31 +22,32 @@ class Population:
         for i in range(pop_size):
             new_indiv = Individual()
             self.elements.append(new_indiv)
-        
+
     def find_best(self):
         """Returns the individual in the population with the best fitness
         """
         best = min(self.elements, key=attrgetter('fitness'))
         return best
-    
+
     def __str__(self):
         return f"Population of size {self.pop_size}"
-    
+
     def evolve(self, n_gens, prob_crossover, prob_mutation,
                selection_algorithm, crossover_algorithm, mutation_algorithm):
         # Iterate over a certain of generations
         # Each iteration:
-            
+        fitness = []
+        constant = []
+
         champion = self.find_best()
         print(f"Initial best fitness: {champion.fitness}")
-        
+
         for gen in range(n_gens):
             new_elements = []
-            
-            # Elitism
-            champion = self.find_best()
-            new_elements.append(champion)
-            
+            # # Elitism
+            # champion = self.find_best()
+            # new_elements.append(champion)
+
             while len(new_elements) < self.pop_size:
                 # Apply Selection
                 p1, p2 = selection_algorithm(self.elements)
@@ -60,22 +62,58 @@ class Population:
                 # Put the new individuals into a new population
                 new_elements.append(child1)
                 new_elements.append(child2)
-                
+
+            fitness.append(champion.fitness)
+            constant.append(champion.representation[-1])
             # Thew new population replaces the old population
             self.elements = new_elements
             champion = self.find_best()
-            print(f"Gen {gen+1} best fitness: {champion.fitness}")
-        return champion
-    
-if __name__ == "__main__":
+            print(f"Gen {gen + 1} best fitness: {champion.fitness}")
 
-    my_pop = Population(pop_size = 121)
-    my_sol = my_pop.evolve(2000, 0.7,0.1,
-        selection_algorithm = tournament_selection,
-        crossover_algorithm = double_point_co,
-        mutation_algorithm = boundary_mutation,
-    )
+        plt.plot(fitness)
+        plt.ylim([30, 40])
+        plt.ylabel('FitnessZoomed')
+        plt.show()
+        plt.plot(fitness)
+        plt.ylabel('Fitness')
+        plt.show()
+        plt.plot(constant)
+        plt.ylabel('Constant')
+        plt.show()
+
+        return champion, fitness
+
+
+def write_txt(fitness, representation, pop_size, gen, c_prob, m_prob, sel_al, cro_al, mut_al, fitness2):
+    with open("GA_results.txt", "a") as f:
+        sep = str("_" * 250) + "\n"
+        par = "Pop_size: " + str(pop_size) + ", " + "Gen number: " + str(gen) + ", " + "Crossover prob: " + str(
+            c_prob) + ", " + "Mutation prob: " + str(m_prob) + "\n"
+        al = "Selection algorithm: " + str(sel_al) + ", " + "Crossover algorithm: " + str(
+            cro_al) + ", " + "Mutation algorithm: " + str(mut_al) + "\n"
+        fit2 = "Fitness at each generation: " + str(fitness2) + "\n"
+        fit = "Best Fitness: " + str(fitness) + "\n"
+        rep = "Best Representation: " + str(representation) + "\n"
+        lis = [sep, par, al, fit2, fit, rep]
+        f.writelines(lis)
+        f.close()
+
+
+if __name__ == "__main__":
+    pop_size = 100
+    gen = 300
+    c_prob = 0.7
+    m_prob = 0.2
+    selection_al = "tournament_selection"  # É preciso mudar isto para o ficheiro
+    crossover_al = "double_point_co"  # É preciso mudar isto para o ficheiro
+    mutation_al = "swap_mutation"  # É preciso mudar isto para o ficheiro
+    my_pop = Population(pop_size=pop_size)
+    my_sol, fit = my_pop.evolve(gen, c_prob, m_prob,
+                                selection_algorithm=tournament_selection,
+                                crossover_algorithm=double_point_co,
+                                mutation_algorithm=swap_mutation,
+                                )
     print(my_sol)
     print(my_sol.representation)
-    
-    
+    write_txt(my_sol.fitness, my_sol.representation, pop_size, gen, c_prob, m_prob, selection_al, crossover_al,
+              mutation_al, fit)
